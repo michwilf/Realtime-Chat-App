47c844faa7031276343b40c7cce54b2d044847ce
Antii <mikeywilfert@gmail.com>
2023-05-31 15:06:09 +0100

    Initial commit

---
+++ src/components/FriendRequests.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import { pusherClient } from '@/lib/pusher'
+import { toPusherKey } from '@/lib/utils'
+import axios from 'axios'
+import { Check, UserPlus, X } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { FC, useEffect, useState } from 'react'
+
+interface FriendRequestsProps {
+    incomingFriendRequests: IncomingFriendRequest[]
+    sessionId: string
+}
+
+const FriendRequests: FC<FriendRequestsProps> = ({
+    incomingFriendRequests,
+    sessionId
+}) => {
+    const router = useRouter()
+    const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
+        incomingFriendRequests 
+    )
+
+    useEffect(() => {
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:incoming_friend_requests`))
+        const friendRequestHandler = ({senderId, senderEmail}: IncomingFriendRequest) => {
+            setFriendRequests((prev) => [...prev, {
+                senderId,
+                senderEmail
+            }])
+        }
+        pusherClient.bind(`incoming_friend_request`, friendRequestHandler)
+
+        return () => {
+            pusherClient.unbind(`incoming_friend_request`, friendRequestHandler)
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:incoming_friend_requests`))
+        }
+    }, [sessionId])
+
+    const acceptFriend = async (senderId: string) => {
+        await axios.post('/api/friends/accept', {
+            id: senderId
+        })
+
+        setFriendRequests((prev) => prev.filter((request) => request.senderId !== senderId))
+
+        router.refresh()
+    }
+
+    const denyFriend = async (senderId: string) => {
+        await axios.post('/api/friends/delete', {
+            id: senderId
+        })
+
+        setFriendRequests((prev) => prev.filter((request) => request.senderId !== senderId))
+
+        router.refresh()
+    }
+
+    return <>
+        {friendRequests.length === 0 ? (
+            <p className='text-zinc-500 text-sm'>Nothing to show here...</p>
+        ) : (
+                friendRequests.map((request) => (
+                    <div key={request.senderId} className='flex items-center gap-4'>
+                        <UserPlus className='text-black' />
+                        <p className='font-medium text-lg'>{request.senderEmail}</p>
+                        <button aria-label="accept friend" onClick={() => acceptFriend(request.senderId)} className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"><Check className='font-semibold text-white w-3/4 h-3/4'/></button><button></button>
+                        <button aria-label="deny friend" onClick={() => denyFriend(request.senderId)} className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"><X className='font-semibold text-white w-3/4 h-3/4'/></button><button></button>
+                    </div>
+                ))
+             )}
+    </>
+}
+
+export default FriendRequests
